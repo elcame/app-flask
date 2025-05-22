d51cd30c4878d39a20355f2bbdc1afd40bcc312d
JOSE CAMELO <CAMELO794@GMAIL.com>
2025-05-22 13:11:48 -0500

    fix: modifica la configuración para gunicorn en Render

--- app.py
+++ app.py
@@ -103,7 +103,9 @@ def create_app():
 
     return app
 
+# Crear la instancia de la aplicación
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
