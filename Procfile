adbc29cf6de1ac23fbd4000984530bf2dfe9d345
JOSE CAMELO <CAMELO794@GMAIL.com>
2025-05-22 21:38:14 -0500

    fix: implementa Procfile y simplifica configuración de despliegue

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app 
+web: python -m gunicorn --config gunicorn_config.py wsgi:app 
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
 from app import app
 
-# Asegurarse de que la aplicación está configurada para producción
-app.config['ENV'] = 'production'
-app.config['DEBUG'] = False
-
 if __name__ == "__main__":
     app.run() 
