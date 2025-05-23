f769e924c0b2e677982530a467a0b281303f76b3
JOSE CAMELO <CAMELO794@GMAIL.com>
2025-05-22 22:42:17 -0500

    fix: quita la relación con Tractocamion en el modelo Seguro

--- models/seguro.py
+++ models/seguro.py
@@ -4,13 +4,10 @@ class Seguro(db.Model):
     __tablename__ = 'seguros'
 
     id = db.Column(db.Integer, primary_key=True)
-    placa = db.Column(db.String(10), nullable=False)  # Quitamos temporalmente la restricción de clave foránea
+    placa = db.Column(db.String(10), nullable=False)
     fecha_inicio = db.Column(db.Date, nullable=False)
     fecha_fin = db.Column(db.Date, nullable=False)
     valor = db.Column(db.Float, nullable=False)
     descripcion = db.Column(db.String(255))
-    
-    tractocamion = db.relationship('Tractocamion', backref=db.backref('seguros', lazy=True))
-    
     
    
